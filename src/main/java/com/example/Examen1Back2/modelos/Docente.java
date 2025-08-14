8fe1c57160178247edfb828a45770ec7f68abecd
LauBermudez <laurabermudezp04@gmail.com>
2025-08-14 18:34:17 -0500

    corrigiendo la clase 'Docente'

--- src/main/java/com/example/Examen1Back2/modelos/Docente.java
+++ src/main/java/com/example/Examen1Back2/modelos/Docente.java
@@ -1,18 +1,24 @@
 package com.example.Examen1Back2.modelos;
+
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.util.List;
 
-@Entit
+@Entity
+@Table(name = "docente")
 public class Docente {
 
+    // ATRIBUTOS
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "especialidad", length = 100, nullable = false, unique = true, updatable = true)
+    private String especialidad;
 
-    private  String especialidad;
+    // RELACIONES
 
     @OneToMany(mappedBy = "docente")
     @JsonManagedReference(value = "docente-curso")
@@ -23,18 +29,47 @@ public class Docente {
     @JsonManagedReference(value = "docente-usuario")
     private Usuario usuario;
 
+    // CONSTRUCTORES
 
+    public Docente() {}
+
+    public Docente(List<Curso> cursos, String especialidad, Integer id, Usuario usuario) {
+        this.cursos = cursos;
+        this.especialidad = especialidad;
+        this.id = id;
+        this.usuario = usuario;
+    }
+
+    // GETTERS
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
 
     public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public String getEspecialidad() {
         return especialidad;
+    }
+
+    // SETTERS
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public void setEspecialidad(String especialidad) {
--- src/main/java/com/example/Examen1Back2/modelos/README.txt
+++ src/main/java/com/example/Examen1Back2/modelos/README.txt
@@ -16,4 +16,14 @@ COMMIT 2: Corrección de la clase 'Curso'.
 3) Agregé la anotación @Column referente a la columna 'nombre' de la tabla y sus respectivos atributos para la BD (l:16).
 4) Corregí un error de sintaxis por falta del ';' (l:17 - l:22). Además, en la línea 17 añadí el modificador de acceso 'private'.
 5) Modifiqué el constructor con parámetros y le añadí el atributo 'docente' (l:31 - l:32).
-6) Generé metodos GETTERS & SETTERS para cada atributo de la clase (l:37 - l:51)
+6) Generé metodos GETTERS & SETTERS para cada atributo de la clase (l:37 - l:51).
+
+COMMIT 3: Corrección de la clase 'Docente'.
+
+1) Corregí errores de sintaxis, como anotaciónes incompletas (@Ent___) (l:8).
+2) Añadí la anotación @Table con el nombre de la respectiva tabla (l:9).
+3) Añadí la anotación @Id para la respectiva clase (l:14).
+4) Añadí la anotación @Column para el atributo 'especialidad' y algunos atributos (l:18).
+5) Creé los dos constructores pertinentes para la clase: vacío y con parámetros (l:34 - l:36).
+6) Creé los métodos GETTER que faltaban y los separé de los setters para tener una mejor visibilidad de los mismos (l:43).
+7) Creé los métodos SETTER que faltaban y los separé de los getter para tener una mejor visibilidad de los mismos (l:61).
